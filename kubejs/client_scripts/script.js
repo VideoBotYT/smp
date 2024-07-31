959203a9b0b377ae70db8fe522382a39e0c25776
VideoBot <lander.meersman@gmail.com>
2024-07-31 16:45:33 +0200

    changed recipes added quests and new mods

---
+++ kubejs/client_scripts/script.js
@@ -0,0 +1,4 @@
+JEIEvents.hideItems(event => {
+    event.hide('ad_astra_giselle_addon:automation_nasa_workbench')
+    event.hide('ad_astra:nasa_workbench')
+})
---
+++ kubejs/server_scripts/recipe.js
@@ -0,0 +1,104 @@
+ServerEvents.recipes(event => {
+    event.shaped(
+        Item.of("minecraft:stick", 16), [
+        'L',
+        'L'
+    ], {
+        L: '#minecraft:logs'
+    })
+
+    event.recipes.createMechanicalCrafting('ad_astra:launch_pad', [
+        'SSS',
+        'SPS',
+        'SSS'
+    ], {
+        S: 'mekanism:block_steel',
+        P: 'create:iron_sheet'
+    })
+    event.recipes.createMechanicalCrafting('ad_astra:tier_1_rocket', [
+        '  N  ',
+        ' PPP ',
+        ' BBB ',
+        ' BSB ',
+        ' BBB ',
+        'FTPTF',
+        'F E F'
+    ], {
+        N: 'ad_astra:rocket_nose_cone',
+        B: 'mekanism:block_steel',
+        S: '#create:seats',
+        F: 'ad_astra:rocket_fin',
+        T: 'ad_astra:steel_tank',
+        E: 'ad_astra:steel_engine',
+        P: 'create:iron_sheet'
+    })
+    event.recipes.createMechanicalCrafting('ad_astra:tier_2_rocket', [
+        '  N  ',
+        ' PVP ',
+        ' BBB ',
+        ' BSB ',
+        ' BBB ',
+        'FTTTF',
+        'F E F'
+    ], {
+        N: 'ad_astra:rocket_nose_cone',
+        B: 'mekanism:block_steel',
+        S: '#create:seats',
+        F: 'ad_astra:rocket_fin',
+        E: 'ad_astra:desh_engine',
+		T: 'ad_astra:desh_tank',
+        P: 'create:iron_sheet',
+        V: 'ad_astra:vent'
+    })
+    event.recipes.createMechanicalCrafting('ad_astra:tier_3_rocket', [
+        '   N   ',
+        '  OOO  ',
+        '  PVP  ',
+        '  BBB  ',
+        '  BSB  ',
+        '  BGB  ',
+        'F BBB F',
+        'FRTTTRF',
+        'E  E  E'
+    ], {
+        N: 'ad_astra:rocket_nose_cone',
+        O: 'ad_astra:ostrum_plate',
+        P: 'create:iron_sheet',
+        V: 'ad_astra:vent',
+        B: 'mekanism:block_steel',
+        E: 'ad_astra:ostrum_engine',
+        T: 'ad_astra:ostrum_tank',
+        F: 'ad_astra:rocket_fin',
+        G: '#forge:glass',
+        S: '#create:seats',
+        R: 'ad_astra:steel_rod'
+    })
+    event.recipes.createMechanicalCrafting('ad_astra:tier_4_rocket', [
+        '    N    ',
+        '   PVP   ',
+        '   BBB   ',
+        '   BSB   ',
+        '   BGB   ',
+        'F  BBB  F',
+        'F  CCC  F',
+        'FRRTTTRRF',
+        'E   E   E'
+    ], {
+        N: 'ad_astra:rocket_nose_cone',
+        P: 'create:iron_sheet',
+        V: 'ad_astra:vent',
+        B: 'mekanism:block_steel',
+        C: 'ad_astra:calorite_plate',
+        E: 'ad_astra:calorite_engine',
+        F: 'ad_astra:rocket_fin',
+        R: 'ad_astra:steel_rod',
+        T: 'ad_astra:calorite_tank',
+        S: '#create:seats',
+        G: '#forge:glass'
+    })
+
+    event.remove({ id: 'ad_astra:nasa_workbench/tier_1_rocket' })
+	event.remove({ id: 'ad_astra:nasa_workbench/tier_2_rocket' })
+	event.remove({ id: 'ad_astra:nasa_workbench/tier_3_rocket' })
+	event.remove({ id: 'ad_astra:nasa_workbench/tier_4_rocket' })
+})
